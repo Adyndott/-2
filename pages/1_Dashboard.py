7bd8dfccf7095d36fd314f3a1835ccf2da45b0fa
Adyndott <dyatmaset@gmail.com>
2025-05-22 09:26:57 +0700

    Update and rename 1_halaman.py to 1_Dashboard.py

---
+++ pages/1_Dashboard.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Iris Dashboard App", layout="centered")
+st.sidebar.header("Dashboard")
+
+st.title("Selamat datang di Aplikasi Streamlit Sederhana")
+st.write("""
+Ini adalah halaman pertamadari Streamlit.
+Gunakan sidebar di kiri untuk menavigasi ke halaman lainnya.
+""")
--- pages/1_halaman.py
+++
@@ -1,9 +0,0 @@
-import.streamlit as st
-
-st.set_page_config(page_title="Halaman Satu Streamlit", layout="wide")
-
-st.title("Selamat Datang di Halaman Pertama Streamlit")
-st.write("""
-Ini adalah halaman pertamadari Streamlit.
-Gunakan sidebar di kiri untuk menavigasi ke halaman lainnya.
-""")
